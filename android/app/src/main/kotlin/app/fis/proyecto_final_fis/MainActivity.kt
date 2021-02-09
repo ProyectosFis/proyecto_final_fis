5afff96bf4fc98605ca7934bee3a2b41efe005f1
Santiago Rios Valero <sriosv@correo.udistrital.edu.co>
2021-02-09 16:57:52 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/app/fis/proyecto_final_fis/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.fis.proyecto_final_fis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_final_fis/splashScreen.dart';
+
+
+
+void main() => runApp(Yourmet());
+
+class Yourmet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+      routes: <String, WidgetBuilder> {
+
+        '/screen4' : (BuildContext context) => SplashScreen()
+      },
+    );
+  }
+}
+
